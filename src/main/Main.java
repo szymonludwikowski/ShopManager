e559964bf0360c969693aac2dc1e84b6cf364953
szymonludwikowski <sz.ludwikowski@gmail.com>
2017-02-14 22:00:38 +0100

    added pac and class

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.text.ParseException;
+
+import menu.Menu;
+
+public class Main {
+
+	public static void main(String[] args) throws ParseException, Exception {
+
+		Menu Menu = new Menu();
+
+		Menu.runApp();
+
+	}
+}
